3db28d81dfdfa5b4c53f7a229e8368256d2a9857
944750720 <944750720@qq.com>
2026-05-23 21:11:04 +0900

    feat: add github icon and site config

---
+++ lib/config.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+	title: "CHJ Blog",
+	description: "A blog built with Next.js",
+	header: {
+		menu: [
+			{ title: "Blog", link: "/blog" },
+			{ title: "Projects", link: "/projects" },
+			{ title: "Links", link: "/links" },
+			{ title: "About", link: "/about" },
+		],
+	},
+	footer: {
+		links: [
+			{ title: "Copyright", link: "/copyright" },
+			{ title: "Privacy", link: "/privacy-policy" },
+		],
+		social: {
+			github: "https://github.com/944750720/blog-frontend",
+		},
+	},
+};
